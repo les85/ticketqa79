3feeb651c06d3544f58de77f4a7a16264180ca6a
lesnoy <bigib@yandex.ru>
2024-02-02 17:33:23 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Стоимость билета
+        int ticketPrice = 200;
+
+        // Рассчитываем количество миль
+        int miles = ticketPrice / 20;
+
+        // Выводим результат
+        System.out.println("За купленный билет начислено " + miles + " миль.");
+    }
+}
